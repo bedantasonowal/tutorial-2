7807448f62eb69e17c10872fbfa18647e706eec6
bedantasonowal <109734483+bedantasonowal@users.noreply.github.com>
2022-07-21 23:33:10 -0800

    Add files via upload

---
+++ project2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+ {
+    double num1, num2, product;
+    cout <<"Enter two numbers: ";
+	
+	//stores two floating point numbers in num1and num2 respectively
+	cin >> num1 >>num2;
+	
+	//performs multiplication and stores the result in productvariable
+	product = num1 * num2;
+	
+	cout <<"product = " << product;
+	 
+	return 0;
+}
